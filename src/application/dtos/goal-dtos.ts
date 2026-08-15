a2ea30a6d02dcea7f9db50b5a629564e08be98c8
Johan <johanrsilveira@gmail.com>
2026-08-14 23:12:24 -0300

    fix: include id field in GoalDto response

--- src/application/dtos/goal-dtos.ts
+++ src/application/dtos/goal-dtos.ts
@@ -1,6 +1,7 @@
 ﻿import { Goal } from "../../domain/entities/goal";
 
 export interface GoalDto {
+    id: string;
     name: string;
     description: string;
     targetAmount: number;
@@ -10,6 +11,7 @@ export interface GoalDto {
 
 export function toGoalDto(goal: Goal): GoalDto {
     return {
+        id: goal.id,
         name: goal.name,
         description: goal.description,
         targetAmount: goal.targetAmount,
